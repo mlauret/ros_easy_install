f65e8c9f35594982e040e13a8658766cd0c1de84
Mathieu LAURET <mathieu.lauret30@gmail.com>
2019-04-01 11:02:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-gksudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+pkexec sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-gksudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+pkexec apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
-gksudo apt-get update
+pkexec apt-get update
 
-gksudo apt-get install -y ros-kinetic-ros-base
+pkexec apt-get install -y ros-kinetic-ros-base
 
-gksudo rosdep init
+pkexec rosdep init
 
 rosdep update
 
