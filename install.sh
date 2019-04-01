d2c0d39288dd761db103bfd23a4e16c7490a3262
Mathieu LAURET <mathieu.lauret30@gmail.com>
2019-04-01 11:13:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-pkexec su
+PASS=`zenity --password`
 
-#
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+echo $PASS | sudo -S sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-#
-apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-#
-apt-get update
-#
-apt-get install -y ros-kinetic-ros-base
-#
-rosdep init
 
-exit
+echo $PASS | sudo -S apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+echo $PASS | sudo -S apt-get update
+
+echo $PASS | sudo -S apt-get install -y ros-kinetic-ros-base
+
+echo $PASS | sudo -S rosdep init
+
 
 rosdep update
 
