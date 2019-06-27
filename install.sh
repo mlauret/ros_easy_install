46a32f86a0a020b21a3d480155d184255aa416a6
Mathieu LAURET <mathieu.lauret30@gmail.com>
2019-06-27 13:23:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ PASS=`zenity --password`
 echo $PASS | sudo -S sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 
-echo $PASS | sudo -S apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+echo $PASS | sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 echo $PASS | sudo -S apt-get update
 
