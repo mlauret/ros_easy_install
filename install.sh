ffda47be5173f5ba437054c392e1ff0b742dc827
Mathieu LAURET <mathieu.lauret30@gmail.com>
2019-10-02 00:01:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-PASS=`zenity --password`
+echo -n Enter Sudo password: 
+read -s PASS
 
 echo $PASS | sudo -S sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
