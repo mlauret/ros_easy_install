a7daef2434dffd246699c729ae43615f53991933
Mathieu LAURET <mathieu.lauret30@gmail.com>
2019-11-04 16:59:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,12 +16,58 @@ read_password_from_cli() {
   )"
 }
 
+read_password_from_file(){
+  read -r firstline<.secret
+}
+
+check_if_sudo(){
+ if [ "$EUID" -e 0 ]; then
+   echo "Please run as normal user"
+   exit 1
+ fi
+}
+
+check_if_source_list_exist(){
+  if [ -e /etc/apt/sources.list.d/ros-latest.list ]; then
+    echo "An another ROS version seem to be installed (/etc/apt/sources.list.d/ros-latest.list) already exist)"
+    exit 1
+  fi
+}
+
+check_if_public_key_exist(){ 
+  apt-key adv --list-public-keys --with-colons | grep '^pub' | cut -d':' -f 5 | egrep F42ED6FBAB17C655 &> /dev/null
+  if [ $? -e 0 ]; then
+    echo "An another ROS version seem to be installed (ROS apt key exist)"
+    exit 1
+  fi
+}
+
+check_if_source_in_bashrc_exist(){
+  grep "source /opt/ros/" ~/.bashrc &> /dev/null
+  if [ $? -e 0 ]; then
+    echo "An another ROS version seem to be installed (source in .bashrc exist)"
+    exit 1
+  fi
+}
+
 do_install(){
 
-  read_password_from_cli
+  check_if_sudo # exit if user is super user
+  
+  if [ -z "$PASS" ] && [ -r .secret ]; then
+    read_password_from_file
+  elif [ -z "$PASS" ] && [ ! -r .secret ]; then
+    read_password_from_cli
+  else
+    # Password is set in env variable, nothing to do
+  fi
+
+  # Check if ROS is already installed by checking some step
+  check_if_source_list_exist
+  check_if_public_key_exist
+  check_if_source_in_bashrc_exist
 
   echo $PASS | sudo -S sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-
 
   echo $PASS | sudo -S apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
