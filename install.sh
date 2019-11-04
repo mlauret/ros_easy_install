bf3edfad3eb9689dc850ae9e0302b10a33ed8535
Mathieu LAURET <mathieu.lauret30@gmail.com>
2019-11-04 14:26:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
-
-stty -echo
-printf "Password: "
-read PASS
-stty echo
-printf "\n"
-
-
-echo $PASS | sudo -S sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+{
+  do_install(){
+  stty -echo
+  printf "Password: "
+  read PASS
+  stty echo
+  printf "\n"
 
 
-echo $PASS | sudo -S apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-
-echo $PASS | sudo -S apt-get update
-
-echo $PASS | sudo -S apt-get install -y ros-kinetic-ros-base
-
-echo $PASS | sudo -S rosdep init
+  echo $PASS | sudo -S sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 
-rosdep update
+  echo $PASS | sudo -S apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+  echo $PASS | sudo -S apt-get update
 
-source ~/.bashrc
+  echo $PASS | sudo -S apt-get install -y ros-kinetic-ros-base
+
+  echo $PASS | sudo -S rosdep init
+
+
+  rosdep update
+
+  echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+
+  source ~/.bashrc
+ }
+
+do_install()
+
+}
